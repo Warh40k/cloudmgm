79179421d17ddd58d415c80fd30507accdd8eed5
nikita <nicita.zinkevitch@yandex.ru>
2024-02-23 23:01:13 +0300

    auth postgres test

---
+++ internal/api/repository/postgres/auth_test.go
@@ -0,0 +1,78 @@
+package postgres
+
+import (
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/Warh40k/cloud-manager/internal/domain"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAuthPostgres_SignUp(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	dbx := sqlx.NewDb(db, "sqlmock")
+
+	r := NewAuthPostgres(dbx)
+	id := uuid.New()
+
+	tests := []struct {
+		name    string
+		mock    func()
+		input   domain.User
+		want    uuid.UUID
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id"}).AddRow(id)
+				mock.ExpectQuery("INSERT INTO users").
+					WithArgs(id, "Test", "test", "$2a$10$7wyI.VRyw8GRxUBp9Gi3b.S7EH6u45HtKeG3GklSkSLtpoceXYAlO").WillReturnRows(rows)
+			},
+			input: domain.User{
+				Id:           id,
+				Name:         "Test",
+				Login:        "test",
+				PasswordHash: "$2a$10$7wyI.VRyw8GRxUBp9Gi3b.S7EH6u45HtKeG3GklSkSLtpoceXYAlO",
+			},
+			want: id,
+		},
+		{
+			name: "Empty Fields",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id"})
+				mock.ExpectQuery(fmt.Sprintf("INSERT INTO %s", usersTable)).
+					WithArgs(id, "Test", "test", "").WillReturnRows(rows)
+			},
+			input: domain.User{
+				Id:       id,
+				Name:     "Test",
+				Login:    "test",
+				Password: "",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+			got, err := r.SignUp(tt.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+
+}
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -7,8 +7,9 @@ import (
 
 type User struct {
 	Id           uuid.UUID `json:"id,omitempty" db:"id"`
-	Name         string    `json:"name" db:"name"`
-	Login        string    `json:"login"`
-	PasswordHash string    `json:"password" db:"password_hash"`
+	Name         string    `json:"name" db:"name" validate:"required"`
+	Login        string    `json:"login" validate:"required"`
+	Password     string    `json:"password" db:"-" validate:"required"`
+	PasswordHash string    `json:"-" db:"password_hash"`
 	Created      time.Time `json:"created" db:"created"`
 }
