3dff96b261db01c118d4f5a6ceba157ac0d07df3
nikita <nicita.zinkevitch@yandex.ru>
2024-02-26 22:47:59 +0300

    add datacenters relation

--- schema/postgresql/000001_init.down.sql
+++ schema/postgresql/000001_init.down.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS users cascade ;
 DROP TABLE IF EXISTS vmachines cascade ;
 DROP TABLE IF EXISTS users_vmachines ;
+DROP TABLE IF EXISTS datacenters ;
--- schema/postgresql/000001_init.up.sql
+++ schema/postgresql/000001_init.up.sql
@@ -4,15 +4,26 @@ CREATE TABLE users
     name varchar(255) not null,
     username varchar(255) not null unique,
     password_hash varchar(255) not null,
+    balance double precision not null default 0,
+    created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE datacenters
+(
+    id uuid primary key,
+    title varchar(255) not null,
+    geolocation varchar(255) not null,
+    total_size double precision not null default 0,
     created timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE vmachines
 (
     id uuid primary key,
-    title varchar(255) not null,
+    datacenter_id uuid references datacenters(id),
+    label varchar(255) not null,
     description varchar(255),
-    status smallint,
+    size int,
     created timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
